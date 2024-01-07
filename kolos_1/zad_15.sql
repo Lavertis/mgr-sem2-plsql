a67d97c8e6226101c75e54d57014bf15012def57
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2024-01-07 14:30:37 +0100

    feat(kolos): zad 15

---
+++ kolos_1/zad_15.sql
@@ -0,0 +1,56 @@
+-- Zadanie 15
+-- Ktory egzaminator nie przeprowadzil egzaminow w poszczegolnych latach? Dla kazdego roku, w ktorym odbyly sie
+-- egzaminy, nalezy wskazac egzaminatora, ktory w danym roku nie prowadzil egzaminow. W odpowiedzi nalezy umiescic dane
+-- o roku (w formacie YYYY) oraz pelne informacje o egzaminatorze (identyfikator, nazwisko, imię). W zadaniu nalezy
+-- wykorzystac technike wyjatkow.
+
+declare
+    NieEgzaminowalException exception;
+    cursor lata_egzaminow is select distinct extract(year from DATA_EGZAMIN) as rok
+                             from EGZAMINY order by rok;
+
+--     cursor egzaminatorzy_egzaminujacy_w_danym_roku(rok number) is
+--         select distinct EGZAMINATORZY.ID_EGZAMINATOR, IMIE, NAZWISKO
+--         from EGZAMINY
+--                  JOIN EGZAMINATORZY ON EGZAMINY.ID_EGZAMINATOR = EGZAMINATORZY.ID_EGZAMINATOR
+--         where extract(year from DATA_EGZAMIN) = rok
+--         order by EGZAMINATORZY.ID_EGZAMINATOR;
+--     cursor egzaminatorzy_nie_egzaminujacy_w_danym_roku(rok number) is
+--         select distinct ID_EGZAMINATOR, IMIE, NAZWISKO
+--         from EGZAMINATORZY
+--         where ID_EGZAMINATOR not in (select distinct ID_EGZAMINATOR
+--                                      from EGZAMINY
+--                                      where extract(year from DATA_EGZAMIN) = rok);
+
+    function czy_egzaminator_egzaminowal_w_roku(id_egzaminatora number, rok number) return boolean is
+        cursor egzaminy_egzaminatora is select distinct ID_EGZAMINATOR
+                                        from EGZAMINY
+                                        where extract(year from DATA_EGZAMIN) = rok
+                                          and ID_EGZAMINATOR = id_egzaminatora;
+    begin
+        for s in egzaminy_egzaminatora
+            loop
+                return true;
+            end loop;
+        return false;
+    end;
+begin
+    for rok_data in lata_egzaminow
+        loop
+            dbms_output.put_line('Rok: ' || rok_data.rok);
+            for egzaminator in (select * from EGZAMINATORZY order by ID_EGZAMINATOR)
+                loop
+                    begin
+                        if not czy_egzaminator_egzaminowal_w_roku(EGZAMINATOR.ID_EGZAMINATOR, rok_data.rok) then
+                            raise NieEgzaminowalException;
+                        end if;
+                    exception
+                        when NieEgzaminowalException then
+                            dbms_output.put_line(
+                                    'Egzaminator: ' || egzaminator.ID_EGZAMINATOR || ' ' ||
+                                    egzaminator.IMIE || ' ' || egzaminator.NAZWISKO
+                            );
+                    end;
+                end loop;
+        end loop;
+end;
